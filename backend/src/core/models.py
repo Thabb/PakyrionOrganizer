156598992fbc5828f2e66d5dd000601fa8f549fc
Thabb <49516328+Thabb@users.noreply.github.com>
2023-03-14 19:33:30 +0100

    ADD: CharacterSerializer

--- backend/src/core/models.py
+++ backend/src/core/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 
 
+class User(models.Model):
+    name = models.CharField(max_length=255)
+
+
 class Character(models.Model):
     name = models.CharField(max_length=255)
     group = models.CharField(max_length=255)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE, default=0)
