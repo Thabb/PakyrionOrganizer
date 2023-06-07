b0bd7ba141a1bdc3894ac0675820fc6ddffe1b62
Thabb <49516328+Thabb@users.noreply.github.com>
2023-06-07 16:17:09 +0200

    REMOVE: Unused function

--- frontend/src/shared/api.js
+++ frontend/src/shared/api.js
@@ -1,22 +1,5 @@
 import axios from 'axios';
 
-/**
- * a
- * @param {string} name
- * @return {string|null}
- */
-function getCookie(name) {
-  const cookieString = document.cookie;
-  const cookies = cookieString.split(';');
-  for (let i = 0; i < cookies.length; i++) {
-    const cookie = cookies[i].trim();
-    if (cookie.startsWith(name + '=')) {
-      return cookie.substring(name.length + 1);
-    }
-  }
-  return null;
-}
-
 axios.defaults.xsrfCookieName = 'csrftoken';
 axios.defaults.xsrfHeaderName = 'HTTP_X_CSRFTOKEN';
 
