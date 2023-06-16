f18fe32759487dec5a124f47d4e8b4972c770341
Thabb <49516328+Thabb@users.noreply.github.com>
2023-06-16 09:09:25 +0200

    ADD: Convention to django-admin

--- backend/src/core/admin.py
+++ backend/src/core/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from core.models import Character, UserData
+from core.models import Character, UserData, Convention
 
 admin.site.register(Character)
 admin.site.register(UserData)
+admin.site.register(Convention)
