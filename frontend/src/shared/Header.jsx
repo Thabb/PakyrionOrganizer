cd6cf85aeec391dfded24a7be335c3340bbafc2f
Thabb <49516328+Thabb@users.noreply.github.com>
2023-06-24 18:55:16 +0200

    ADD; user Icon

--- frontend/src/shared/Header.jsx
+++ frontend/src/shared/Header.jsx
@@ -1,7 +1,8 @@
 import { Link } from 'react-router-dom';
 import logo from '../resources/pakyrion-logo.png';
-import { Container } from 'react-bootstrap';
+import { Col, Container, Row } from 'react-bootstrap';
 import PropTypes from 'prop-types';
+import userIcon from '../resources/user_icon.png';
 
 /**
  * a
@@ -12,16 +13,30 @@ export default function Header({ username, isAdmin }) {
     <>
       <div className="header">
         <Container className="header-container">
-          <Link to={'/'}>
-            <img src={logo} />
-          </Link>
-          <span>
-            Eingeloggt als: {username === 'AnonymousUser' ? '-' : username}
-            <div>
-              <Link to={'/user/'}>User Profil</Link>
-            </div>
-            <div>{isAdmin ? <Link to={'/admin/'}>Admin Seite</Link> : null}</div>
-          </span>
+          <Row>
+            <Col>
+              <Link to={'/'}>
+                <img src={logo} />
+              </Link>
+            </Col>
+            <Col>
+              <Container>
+                <Row className="justify-content-end">
+                  <Col className="col-3">
+                    <div>{isAdmin ? <Link to={'/admin/'}>Admin Seite</Link> : null}</div>
+                  </Col>
+                  <Col className="col-3">
+                    <div>
+                      <Link to={'/user/'}>
+                        <img src={userIcon} width="32" height="32" />{' '}
+                        {username === 'AnonymousUser' ? '-' : username}
+                      </Link>
+                    </div>
+                  </Col>
+                </Row>
+              </Container>
+            </Col>
+          </Row>
         </Container>
       </div>
     </>
